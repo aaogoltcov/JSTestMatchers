43e13d1d7653ef2491b9e662d31e746695ec16ac
Aleksei Ogoltcov <aaogoltcov@mail.ru>
2020-11-04 19:48:26 +0300

    updated

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
-// TODO: write your code here
-import sum from './basic';
+import sort from './sort';
 
-console.log('worked');
-
-console.log(sum([1, 2]));
+console.log(sort([
+  { name: 'мечник', health: 10 },
+  { name: 'маг', health: 100 },
+  { name: 'лучник', health: 80 },
+]));
